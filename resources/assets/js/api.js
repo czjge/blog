23a73113d758933ebdec0d77d1c4ab586ee80116
nevermore_wj <1159617259@qq.com>
2019-01-23 09:44:39 +0800

    xxx

--- resources/assets/js/api.js
+++ resources/assets/js/api.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 export default {
     // 首页
     getIndex: function (params) {
-        return axios.get('http://localhost/api/index', {
+        return axios.get('/api/index', {
             params: params
         })
     },
     // 文章详情
     getPostDetail: function (id) {
-        return axios.get('http://localhost/api/post/' + id)
+        return axios.get('/api/post/' + id)
     }
 }
