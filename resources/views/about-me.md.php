f2ad1f5958ed0e7ce5929ab2c6c2c0ec774774f5
nevermore_wj <1159617259@qq.com>
2019-04-22 15:16:08 +0800

    xxx

--- resources/views/about-me.md.php
+++ resources/views/about-me.md.php
@@ -1,4 +1,100 @@
-###### 十年姆爷老粉，翻唱致敬eminem
+##### 基本信息
 
-- lose yourseself: https://kg2.qq.com/node/play?s=zo6-axzGVqu6ezFT&shareuid=61959f822c24338f33&topsource=a0_pn201001006_z1_u382799447_l1_t1555553256__
-- stan: https://kg2.qq.com/node/play?s=46oB_J4cpGEBl44v&shareuid=61959f822c24338f33&topsource=a0_pn201001006_z1_u382799447_l1_t1555553364__
+姓名：王江                                                 性别：男
+
+出生时间：1990,9                                     籍贯：四川崇州
+
+学历：本科                                                 毕业院校：重庆大学
+
+手机：18161214501                                 邮箱：20097018@cqu.edu.cn
+
+
+
+##### 技能描述
+
+- 熟悉php。掌握laravel、thinkphp框架，了解symphony框架（edusoho）。
+- 了解python。
+- 使用python做服务端任务脚本；
+- 使用scrapy框架爬取简单数据；
+- 使用numpy、pandas和matplotlib等库进行简单数据处理；
+- 了解golang。
+- 使用golang做服务端任务脚本；
+- 熟悉html、css、js基本使用。
+- 了解vue。
+- 熟悉mysql使用和优化。
+- 熟悉redis等常用缓存技术。
+- 熟悉rabbitmq消息队列使用，用其做基本队列、缓存队列和延时队列等。
+- 熟悉常用lamp、lnmp环境。
+- 了解docker、k8s等容器技术。
+- 熟悉git和svn等代码版本控制工具。
+- 熟练阅读英文文档，Stack Overflow重度用户。
+
+
+
+##### 工作经历
+
+- 2016,06-至今
+- 公司：四川精诚名医医疗股份有限公司（成都商报旗下）
+- 职位：php开发
+- 工作内容：负责php相关
+- 2015,11-2016,06（项目解散）
+- 公司：苏州敏行医学信息技术有限公司
+- 职位：php开发
+- 工作内容：
+- 细化并分配任务，控制项目进度
+- 编写核心代码
+- 协助组员解决开发中遇到的问题
+
+- 2015,06-2015,11（项目解散）
+- 公司：四川希望教育集团
+- 职位：php开发
+- 工作内容：在线教育平台二次开发（edusoho）
+- 2014,10-2015,06（项目解散）
+- 公司：四川极智科技有限公司
+- 职位：php开发
+- 工作内容：极客旅游平台开发
+
+- 2014,04-2014,09（回成都）
+- 公司：重庆名爵科技有限公司
+- 职位：php开发
+- 工作内容：CMS和ERP系统开发
+
+
+
+##### 项目经历
+
+- PPT展示系统
+- 介绍：一个灵巧方便的会议辅助系统，由控制端和被控制端组成。
+- 技术栈：
+- php提供api
+- golang和nodejs组成的桥作为UI（https://github.com/Equanox/gotron）
+
+- 诊途app
+
+- 介绍：类似好大夫app，提供线上问诊和病友圈管理等功能，包括医生端和用户端。
+- 技术栈：
+- php提供api
+- rabbitmq传递订单相关事件，python脚本做服务端守护进程运行消费者程序
+- 客户端推送使用了融云（1版）和环信（2版）
+- 图数据库（neo4j）处理医生和患者之间的逻辑关系
+- 下载链接：https://mp.doctorofsc.cn/appdownload/download.html
+
+- 微信投票系统
+- 介绍：功能完备的微信投票系统，经多次大型线上投票活动检验，PV破千万。
+- 项目地址：https://github.com/czjge/wechat-vote
+
+- OSCE考试系统
+
+- 介绍：医学院、医院或医疗机构的模拟临床场景的考试系统。考生通过腕表引导进入
+
+​            考场，考官持pad考试，考试结果后台自动分析。
+
+- 若干其他CMS和CRM系统
+
+
+
+##### 其他
+
+- github：https://github.com/czjge/
+- 个人网站：https://www.czjge.cn
+
